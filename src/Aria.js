aba11f3b373f1b5483a7adcb99162b4ab5c42ab0
alow2438 <aurelialowther@gmail.com>
2022-05-21 12:18:43 -0500

    code blocks implemented and working

--- src/Aria.js
+++ src/Aria.js
@@ -1,38 +1,61 @@
 import * as React from "react";
-const hljs = require('highlight.js/lib/core');
-hljs.registerLanguage('js', require('highlight.js/lib/languages/xml'));
+import { CodeBlock, nord } from "react-code-blocks"
+import "./Aria.scss"
 
-const highlightedCode = hljs.highlight('<span>Hello World!</span>', {language: 'js'}).value
+const code1 =(
+  'text',
+  `<button aria-label="Blue" aria-labelledby="color">
+      Red
+    </button>
+  <span id="color">Yellow</span>
+`
+);
 
+const code2 = (
+  'text',
+  `<button>Yellow</button>`
+);
 
-const Snippet = () => {
-  return (
-    <pre>
-      <code>
-        
-        {highlightedCode}
-      </code>
-    </pre>
-  );
-};
+const code3 = (
+  'text',
+  `<button aria-label="Close" onclick="myDialog.close()">
+  <svg aria-hidden="true" focusable="false" width="17"
+  height="17" xmlns="http://www.w3.org/2000/svg">
+  <path d="m.967 14.217 5.8-5.906-5.765-5.89L3.094.26l5.783
+  5.888L14.66.26l2.092 2.162-5.766 5.889 5.801 5.906-2.092
+  2.162-5.818-5.924-5.818 5.924-2.092-2.162Z" fill="#000"/></svg>
+  </button>`
+);
+
 
 function Aria() {
   return (
-    <div>
+    <div >
       <h1>What is the button's accessible name?</h1>
-      <div className="aria-btn-ex-1">
-        <button aria-label="Blue" aria-labelledby="color">
-            Red
+      <div className="aria-container">
+        <div className="aria-div aria-btn-ex-1">
+          <button aria-label="Blue" aria-labelledby="color">
+              Red
+            </button>
+          <span id="color"> Yellow</span>
+        </div>
+        <br></br>
+        <CodeBlock className="codeBlock" theme={nord} language={'javascript'} text={(code1)} />
+        <br></br>
+        <div className="aria-div aria-btn-ex-2">
+          <button>Yellow</button>
+          <br></br>
+          <CodeBlock className="codeBlock" theme={nord} language={'javascript'} text={(code2)} />
+          <br></br>
+        </div>
+        <br></br>
+        <div className="aria-btn-ex-3">
+          <button aria-label="Close" onclick="myDialog.close()">
+          <svg aria-hidden="true" focusable="false" width="17" height="17" xmlns="http://www.w3.org/2000/svg"><path d="m.967 14.217 5.8-5.906-5.765-5.89L3.094.26l5.783 5.888L14.66.26l2.092 2.162-5.766 5.889 5.801 5.906-2.092 2.162-5.818-5.924-5.818 5.924-2.092-2.162Z" fill="#000"/></svg>
           </button>
-        <span id="color">Yellow</span>
-      </div>
-      <div className="aria-btn-ex-2">
-        <button>Yellow</button>
-      </div>
-      <div className="aria-btn-ex-3">
-        <button aria-label="Close" onclick="myDialog.close()">
-        <svg aria-hidden="true" focusable="false" width="17" height="17" xmlns="http://www.w3.org/2000/svg"><path d="m.967 14.217 5.8-5.906-5.765-5.89L3.094.26l5.783 5.888L14.66.26l2.092 2.162-5.766 5.889 5.801 5.906-2.092 2.162-5.818-5.924-5.818 5.924-2.092-2.162Z" fill="#000"/></svg>
-        </button>
+          <br></br>
+          <CodeBlock className="codeBlock" theme={nord} language={'javascript'} text={(code3)} wrapLongLines={true}/>
+        </div>
       </div>
     </div>
   );
